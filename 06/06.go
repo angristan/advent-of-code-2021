75a6d0d4a307d541aed2805844b3d9d926eb657a
Stanislas Lange <stanislas.lange@pm.me>
2021-12-06 13:23:38 +0100

    Complete day 6

---
+++ 06/06.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"advent-of-code-2021/utils"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func reproduce(fishes map[int]int, days int) int {
+	for i := 0; i < days; i++ {
+		// Keep count of fishes ready to reproduce
+		last0 := fishes[0]
+
+		// Decrease reproduce timer
+		for k := 0; k <= 7; k++ {
+			fishes[k] = fishes[k+1]
+		}
+		// Add new fishes
+		fishes[8] = last0
+		// Reset fishes that have reproduced
+		fishes[6] += last0
+	}
+
+	totalFishes := 0
+	for _, v := range fishes {
+		totalFishes += v
+	}
+
+	return totalFishes
+}
+
+func part1(params utils.RunParams) {
+	// each fish as a single number that represents
+	// the number of days until it creates a new lanternfish.
+	fishes := getInput(params)
+
+	// Problem:
+	// How many lanternfish would there be after 80 days?
+	numberOfDays := 80
+
+	reproducedFishesCount := reproduce(fishes, numberOfDays)
+
+	fmt.Println(reproducedFishesCount)
+}
+
+func part2(params utils.RunParams) {
+	// each fish as a single number that represents
+	// the number of days until it creates a new lanternfish.
+	fishes := getInput(params)
+
+	// Problem:
+	// How many lanternfish would there be after 256 days?
+	numberOfDays := 256
+
+	reproducedFishesCount := reproduce(fishes, numberOfDays)
+
+	fmt.Println(reproducedFishesCount)
+}
+
+func main() {
+	part1(utils.RunParams{Sample: true})
+	part1(utils.RunParams{Sample: false})
+	part2(utils.RunParams{Sample: true})
+	part2(utils.RunParams{Sample: false})
+}
+
+func getInput(params utils.RunParams) map[int]int {
+	lines, err := utils.ReadFileToString("06", params)
+	if err != nil {
+		panic(err)
+	}
+
+	result := make(map[int]int)
+	numbersStr := strings.Split(lines[0], ",")
+	for _, numberStr := range numbersStr {
+		number, err := strconv.Atoi(numberStr)
+		if err != nil {
+			panic(err)
+		}
+		result[number]++
+	}
+
+	return result
+}
